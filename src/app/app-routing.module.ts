6ce0b54b7d5fee9937cbdcf06febb15d36b2460a
ALAND879 <adjr879@gmail.com>
2023-10-22 17:01:20 -0600

    new file:   src/app/Components/agregar-usuario/agregar-usuario.component.css 	new file:   src/app/Components/agregar-usuario/agregar-usuario.component.html 	new file:   src/app/Components/agregar-usuario/agregar-usuario.component.spec.ts 	new file:   src/app/Components/agregar-usuario/agregar-usuario.component.ts 	new file:   src/app/Components/home/home.component.css 	new file:   src/app/Components/home/home.component.html 	new file:   src/app/Components/home/home.component.spec.ts 	new file:   src/app/Components/home/home.component.ts 	modified:   src/app/Components/navbar/navbar.component.html 	modified:   src/app/app-routing.module.ts 	modified:   src/app/app.module.ts 	modified:   src/styles.css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Components/home/home.component';
+import { AgregarUsuarioComponent } from './Components/agregar-usuario/agregar-usuario.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'Home', component: HomeComponent },
+  { path: 'AgregarUsuario', component:AgregarUsuarioComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+  
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './Components/navbar/navbar.component';
+import { HomeComponent } from './Components/home/home.component';
+import { AgregarUsuarioComponent } from './Components/agregar-usuario/agregar-usuario.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    AgregarUsuarioComponent
   ],
   imports: [
     BrowserModule,
